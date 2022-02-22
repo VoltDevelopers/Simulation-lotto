f0184f186e18c858fe50c88dcdc5f644bd487adc
drbrx <80510jb@gmail.com>
2022-02-22 16:55:16 +0100

    Strutturata al classe database, implementati alcuni metodi | v0.2| @drbrx

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/data/Settings.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/data/Settings.java
@@ -1,4 +1,28 @@
 package com.voltdevelopers.lotto.data;
 
 public class Settings {
+
+    private static Settings instance = null;
+    private int nOfPulls;
+
+    private Settings(){
+
+    }
+
+    public static Settings get(){
+
+        if(instance != null)
+            return instance;
+        instance = new Settings();
+        return instance;
+    }
+
+    public int getnOfPulls() {
+        return nOfPulls;
+    }
+
+    public void setnOfPulls(int nOfPulls) {
+        this.nOfPulls = nOfPulls;
+    }
+
 }
