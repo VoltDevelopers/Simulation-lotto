c7ac0a4229d0809471818805e9b6e70f11a694fa
WlanKasper <asu.artur.smirnov@gmail.com>
2022-03-04 01:44:11 +0100

    Added 2 activitys | @WlanKasper

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/MainActivity.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/MainActivity.java
@@ -2,14 +2,45 @@ package com.voltdevelopers.lotto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.voltdevelopers.lotto.activitys.PatternGameActivity;
+import com.voltdevelopers.lotto.activitys.StatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button standardGame, patternGame, stat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findRes();
 
+//        standardGame.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
+
+        patternGame.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PatternGameActivity.class);
+            startActivity(intent);
+        });
+
+        stat.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, StatActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    private void findRes() {
+        standardGame = findViewById(R.id.btn1);
+        patternGame = findViewById(R.id.btn2);
+        stat = findViewById(R.id.btn3);
     }
 }
---
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/activitys/PatternGameActivity.java
@@ -0,0 +1,16 @@
+package com.voltdevelopers.lotto.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.voltdevelopers.lotto.R;
+
+public class PatternGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pattern_game);
+    }
+}
