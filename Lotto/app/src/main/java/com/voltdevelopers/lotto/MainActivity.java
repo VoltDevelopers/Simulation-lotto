2bf1b842b2cffe7cb5b604095d9a0da280214deb
WlanKasper <asu.artur.smirnov@gmail.com>
2022-03-05 00:02:06 +0100

    Added MPAndroidChart | @WlanKasper

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/MainActivity.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/MainActivity.java
@@ -1,13 +1,12 @@
 package com.voltdevelopers.lotto;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.voltdevelopers.lotto.activitys.PatternGameActivity;
 import com.voltdevelopers.lotto.activitys.StatActivity;
--- Lotto/app/src/main/java/com/voltdevelopers/lotto/activitys/PatternGameActivity.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/activitys/PatternGameActivity.java
@@ -4,13 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.voltdevelopers.lotto.R;
 
+import java.util.ArrayList;
+
 public class PatternGameActivity extends AppCompatActivity {
+
+    private static final String TAG = "PatternGameActivity";
+    private LineChart myChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pattern_game);
+
+        myChart = (LineChart) findViewById(R.id.stat1);
+
+        myChart.setDragEnabled(true);
+        myChart.setScaleEnabled(false);
+        ArrayList<Entry> yValues = new ArrayList<>();
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 50f));
+        yValues.add(new Entry(2, 20f));
+        yValues.add(new Entry(3, 90f));
+
+        LineDataSet set_1 = new LineDataSet(yValues, "Data set 1");
+        set_1.setFillAlpha(110);
+        ArrayList<ILineDataSet> dataSets= new ArrayList<>();
+        dataSets.add(set_1);
+
+        LineData data = new LineData(dataSets);
+        myChart.setData(data);
+
     }
 }
