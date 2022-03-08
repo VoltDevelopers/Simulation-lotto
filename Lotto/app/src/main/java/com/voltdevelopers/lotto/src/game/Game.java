7588c9ed7da513dd7299e8e4fd7ca48c4e1e5ba0
WlanKasper <asu.artur.smirnov@gmail.com>
2022-03-08 19:03:22 +0100

    Added commented struct | @WlanKasper

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/src/game/Game.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/src/game/Game.java
@@ -2,4 +2,9 @@ package com.voltdevelopers.lotto.src.game;
 
 public class Game {
 
+    // Func GameLoop(int gnum)
+    // Func PreGameLoop(int gnum)
+    // Func sendAllData()
+    // Func getAllData()
+
 }
