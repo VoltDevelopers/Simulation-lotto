05c765faf882150ad30215d6336c1cff57669e90
WlanKasper <asu.artur.smirnov@gmail.com>
2022-03-08 19:58:16 +0100

    Console + Logs | v0.1 | @WlanKasper

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/layout/Console.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/layout/Console.java
@@ -1,9 +1,47 @@
 package com.voltdevelopers.lotto.layout;
 
+import android.util.Log;
+import java.util.Scanner;
+
 public class Console {
-    public void printStr(String str) {
-        System.out.println(str);
+
+    private static Console instance = null;
+    Scanner input;
+
+    private Console() {
+        input = new Scanner(System.in);
+        // add file for logs
     }
 
-    public void printExp(String exp) { System.out.println("EXCEPTION: " + exp); }
+    public static Console getInstance () {
+        if (instance != null)
+            return instance;
+        instance = new Console();
+        return instance;
+    }
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    public void printStr(String str) {
+        System.out.println(str);
+        Log.i(TAG, str);
+    }
+
+    public void printExp(Exception exp) {
+        exp.printStackTrace();
+        Log.e(TAG, "EXCEPTION", exp);
+    }
+
+    public String getStr(){
+        String input = new Scanner(System.in).next();
+        printStr(input);
+        return input;
+    }
+
+    public int getInt(){
+        int input = new Scanner(System.in).nextInt();
+        printStr(String.valueOf(input));
+        return input;
+    }
+
 }
