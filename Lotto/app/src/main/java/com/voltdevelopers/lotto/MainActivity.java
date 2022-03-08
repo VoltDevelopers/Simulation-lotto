b77711d34cee456f10fa6de1519e202e2df528d5
Arthur <73994817+WlanKasper@users.noreply.github.com>
2022-03-08 18:28:02 +0100

    Base UI + Intents (#18)
    * Base UI Version of Menu | @WlanKasper
    
    * Added 2 activitys | @WlanKasper
    
    * Added test LogCat | @WlanKasper
    
    * Added MPAndroidChart | @WlanKasper

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/MainActivity.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/MainActivity.java
@@ -1,15 +1,53 @@
 package com.voltdevelopers.lotto;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.voltdevelopers.lotto.activitys.PatternGameActivity;
+import com.voltdevelopers.lotto.activitys.StatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button standardGame, patternGame, stat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findRes();
 
+//        standardGame.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
+
+        patternGame.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PatternGameActivity.class);
+            startActivity(intent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Log.i("INFO", "Started Activity" + intent.getIdentifier());
+            }
+        });
+
+        stat.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, StatActivity.class);
+            startActivity(intent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Log.i("INFO", "Started Activity" + intent.getIdentifier());
+            }
+        });
+
+    }
+
+    private void findRes() {
+        standardGame = findViewById(R.id.btn1);
+        patternGame = findViewById(R.id.btn2);
+        stat = findViewById(R.id.btn3);
     }
 }
