63dd66aee16c5e9961ffccbf9637e7e385905901
WlanKasper <asu.artur.smirnov@gmail.com>
2022-03-10 18:24:40 +0100

    Admin

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/data/Database.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/data/Database.java
@@ -6,11 +6,10 @@ public class Database {
 
     private static Database instance = null;
     private Profile[] players; //i 5 giocatori, ordinati come nel pptx con le istruzioni
-    private int gameCounter;
-    private ArrayList<int[]> rounds;
-    private int[] pullsPerNumber; //n di estrazioni per valore (n di estrazioni di 1 si trova nella cella 0, di 2 nella 1, etc etc)
-    private ArrayList<Integer> pullChronology; //ordine estrazioni (ultimo estratto sta all'indice massimo)
-    private String log;             //log usato per la console
+    private final int gameCounter;
+    private final ArrayList<int[]> rounds;
+    private final int[] pullsPerNumber; //n di estrazioni per valore (n di estrazioni di 1 si trova nella cella 0, di 2 nella 1, etc etc)
+    private final ArrayList<Integer> pullChronology; //ordine estrazioni (ultimo estratto sta all'indice massimo)
 
     private Analisys analisys;
 
@@ -25,12 +24,7 @@ public class Database {
 
         pullsPerNumber = new int[Settings.N_NUMBERS];
         for (int i : pullsPerNumber) pullsPerNumber[i] = 0; //inizializzo array
-        pullChronology = new ArrayList<Integer>();
-
-        log = "";
-
-        //TODO add log edit
-
+        pullChronology = new ArrayList<>();
     }
 
     public static Database get() {
@@ -42,73 +36,50 @@ public class Database {
 
     }
 
-    private void initPlayers(){
-
+    private void initPlayers() {
         players = new Profile[Settings.N_PLAYERS];
-        for(int i = 0; i < Settings.N_PLAYERS; i++){
-
+        for (int i = 0; i < Settings.N_PLAYERS; i++) {
             players[i] = new Profile(Settings.PLAYER_NAMES[i]);
-
         }
-        //TODO add log edit
-
     }
 
-    public double getPlayerMoneyWon(int playerN){
-
+    public double getPlayerMoneyWon(int playerN) {
         return players[playerN].getMoneyWon();
-        //TODO add log edit
-
     }
 
-    public double getPlayerMoneySpent(int playerN){
-
+    public double getPlayerMoneySpent(int playerN) {
         return players[playerN].getMoneySpent();
-        //TODO add log edit
-
     }
 
-    public double getPlayerNet(int playerN){
-
+    public double getPlayerNet(int playerN) {
         return players[playerN].getNet();
-        //TODO add log edit
-
     }
 
-    public int getPlayerWins(int playerN){
-
+    public int getPlayerWins(int playerN) {
         return players[playerN].getNWins();
-        //TODO add log edit
-
     }
 
     public void addPull(int[] input) {
-
         rounds.add(input);
         for (int i : input) { //copio i valori ricevuti e ne aumento il numero di estrazioni
             pullsPerNumber[input[i] - 1]++;
             analisys.modChronology(input[i]);
-            //TODO add log edit
         }
 
     }
 
-    public void addPlayerBet(int playerN, int[] input){
-
+    public void addPlayerBet(int playerN, int[] input) {
         players[playerN].addBet(input);
-
     }
 
     public int getPullCount() {
         return gameCounter;
-        //TODO add log edit
     }
-
 
 
     //----------------------analisys methods--------------------------------------------------------
 
-    public int[] getNMostFrequent(int nRequested){
+    public int[] getNMostFrequent(int nRequested) {
         return analisys.getNMostFrequent(nRequested);
     }
 
@@ -117,155 +88,101 @@ public class Database {
     }
 
 
-    public int[] getOldestN(int nRequested){
+    public int[] getOldestN(int nRequested) {
         return analisys.getOldestN(nRequested);
     }
 
     //-----------------------log managment----------------------------------------------------------
+    // TODO: 10/03/22 Add as toString 
+    private class Analisys {
 
-    private void addToString(String in) { //aggiunge un azione al log
-
-        log += in + "\n";
-        //TODO add log edit
-
-    }
-
-    public String pushString() { //ritorna il log senza pulirlo
-
-        return log;
-        //TODO add log edit
-
-    }
-
-    public String pushString(boolean clear) { //ritorna il log, pulisce se input vero
-
-        if (clear) {
-
-        }
-        String out = log;
-        log = "";
-        return out;
-        //TODO add log edit
-
-    }
-
-    private class Analisys{
-
-        Analisys(){};
-
-        public int[] getLatestN(int nRequested){
-
+        public int[] getLatestN(int nRequested) {
             int[] output = new int[nRequested];
             int chronoSize = pullChronology.size();
-            for (int i : output){
-
+            for (int i : output) {
                 output[i] = pullChronology.get(chronoSize);
                 chronoSize--;
-
             }
-            //TODO add log edit
+            //TODO add log
             return output;
-
         }
 
-        private void modChronology(int n){
-
-            if(pullChronology.contains(n))
+        private void modChronology(int n) {
+            if (pullChronology.contains(n))
                 pullChronology.remove(pullChronology.lastIndexOf(n));
 
             pullChronology.add(n);
-
+            //TODO add log
         }
 
-        public int[] getOldestN(int nRequested){
-
+        public int[] getOldestN(int nRequested) {
             int[] output = new int[nRequested];
-            for (int i : output){
-
+            for (int i : output) {
                 output[i] = pullChronology.get(i);
-
             }
 
-            //TODO add log edit
+            //TODO add log
             return output;
-
         }
 
-        public int[] getNMostFrequent(int nRequested){
-
+        public int[] getNMostFrequent(int nRequested) {
             int[] output = new int[nRequested];
-            for (int i : output){
-
+            for (int i : output) {
                 int max = 0;
-                for (int j : pullsPerNumber){
-                    if(pullsPerNumber[j] >= max && !intArrayContains(output, j+1)) {
+                for (int j : pullsPerNumber) {
+                    if (pullsPerNumber[j] >= max && !intArrayContains(output, j + 1)) {
                         max = pullsPerNumber[j];
-                        output[i] = j+1;
+                        output[i] = j + 1;
                     }
                 }
             }
-
-            //TODO add log edit
-
+            //TODO add log
             return output;
         }
 
-        private boolean intArrayContains(int[] arr, int n){
-
+        private boolean intArrayContains(int[] arr, int n) {
             for (int i : arr) {
                 if (arr[i] == n)
+                    //TODO add log
                     return true;
             }
-
+            //TODO add log
             return false;
         }
-
     }
-
 }
 
 class Profile {
-
     private String name;
     private double moneyWon, moneySpent;
-    private int nWins;
-    private ArrayList <int[]> betList;
+    private final int nWins;
+    private final ArrayList<int[]> betList;
 
     public Profile() {
-
         moneyWon = 0;
         moneySpent = 0;
         nWins = 0;
-        betList = new ArrayList<int[]>();
-
+        betList = new ArrayList<>();
     }
 
     public Profile(String name) {
-
         this();
         this.name = name;
-
     }
 
     public void addBet(int[] input) {
-
         betList.add(new int[Settings.get().getnOfPulls()]);
-        for (int i : betList.get(betList.size() - 1)) { //copio i valori ricevuti e ne aumento il numero di estrazioni
+        for (int i : betList.get(betList.size() - 1)) {
             betList.get(betList.size() - 1)[i] = input[i];
         }
-
     }
 
-    public int[] getLastBet(){
-
-        return betList.get(betList.size()-1);
-
+    public int[] getLastBet() {
+        return betList.get(betList.size() - 1);
     }
 
-    public int[] getSelectedBet(int n){
-
+    public int[] getSelectedBet(int n) {
         return betList.get(n);
-
     }
 
     public double getMoneyWon() {
@@ -297,9 +214,8 @@ class Profile {
     }
 
     public String getName() {
-        if(name != null)
+        if (name != null)
             return name;
         return "name not set";
     }
-
 }
