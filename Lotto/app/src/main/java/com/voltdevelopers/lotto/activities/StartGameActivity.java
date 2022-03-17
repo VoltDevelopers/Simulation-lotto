b07400d166a7b9141d6d0465434a149dddd5ef91
enrico visentin <enrico04v@gmail.com>
2022-03-17 18:27:01 +0100

    added 5 lines to graph | v 0.3 | @enry04

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/activities/StartGameActivity.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/activities/StartGameActivity.java
@@ -2,6 +2,7 @@ package com.voltdevelopers.lotto.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -13,6 +14,7 @@ import com.voltdevelopers.lotto.R;
 import com.voltdevelopers.lotto.data.Database;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class StartGameActivity extends AppCompatActivity {
 
@@ -30,45 +32,49 @@ public class StartGameActivity extends AppCompatActivity {
         myChart.setDragEnabled(true);
         myChart.setScaleEnabled(false);
 
-//        ArrayList<Entry> yValues = new ArrayList<>();
-//        yValues.add(new Entry(0, 60f));
-//        yValues.add(new Entry(1, 50f));
-//        yValues.add(new Entry(2, 20f));
-//        yValues.add(new Entry(3, 90f));
-//
-//        LineDataSet set_1 = new LineDataSet(yValues, "Player 1 winnings");
-//        set_1.setFillAlpha(110);
-//        ArrayList<ILineDataSet> dataSets= new ArrayList<>();
-//        dataSets.add(set_1);
-//
-//        LineData data = new LineData(dataSets);
-//        myChart.setData(data);
-
         db = Database.getInstance();
         addDataToGraph();
     }
 
     private void addDataToGraph(){
-        ArrayList<Entry> yValues = new ArrayList<>();
 
-        for (int i = 0; i < db.getSizeAllPulls(); i++) {
+        int colors[] = {Color.RED,Color.YELLOW,Color.WHITE,Color.GREEN,Color.BLUE};
+        ArrayList<ArrayList<Entry>> yValues = new ArrayList<>();
+        ArrayList<LineDataSet> lineDataSets = new ArrayList<>();
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        Random random = new Random();
+        int range = 5;// roba temporanea per i dati dell ' asse y
 
-            // ASSE Y
-            for (int j = 0; j < 5; j++) {
-                int Y = 0;
-                int[] arr = db.getPlayerLastBet(j);
-                for (int m = 0; m < arr.length; m++) {
-                    Y += arr[m];
-                }
-                yValues.add(new Entry(i, Y));
-                LineDataSet lineDataSet = new LineDataSet(yValues, "Player " + i + "winnings");
-                lineDataSet.setFillAlpha(110);
-                ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-                dataSets.add(lineDataSet);
+        for(int i = 0; i < 5; i++){ //ciclo per le 5 linee
 
-                LineData data = new LineData(dataSets);
-                myChart.setData(data);
+            yValues.add(new ArrayList<>());
+
+            for(int j = 0; j < db.getSizeAllPulls(); j++){ //ciclo per scorrere tutte le vincite del singolo giocatore
+
+                //asse X = j
+                int y = 0; // asse y;
+                y = random.nextInt(range);
+                yValues.get(i).add(new Entry(j,y));
+
             }
+
+            lineDataSets.add(new LineDataSet(yValues.get(i), "player " + i + " winnings"));
+            lineDataSets.get(i).setFillAlpha(110);
+            lineDataSets.get(i).setLineWidth(3f);
+            lineDataSets.get(i).setDrawCircles(true);
+            lineDataSets.get(i).setCircleRadius(6);
+            lineDataSets.get(i).setCircleHoleRadius(6);
+            lineDataSets.get(i).setValueTextSize(6);
+            lineDataSets.get(i).setColor(colors[i]);
+            lineDataSets.get(i).setCircleColor(colors[i]);
+            lineDataSets.get(i).setValueTextColor(colors[i]);
+            dataSets.add(lineDataSets.get(i));
+
         }
+
+        //LineData data = new LineData(lineDataSets.get(0),lineDataSets.get(1),lineDataSets.get(2),lineDataSets.get(3),lineDataSets.get(4));
+        LineData data = new LineData(dataSets);
+        myChart.setData(data);
+
     }
 }
