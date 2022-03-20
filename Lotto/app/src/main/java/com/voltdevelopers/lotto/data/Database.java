9c5780de6e402274e8a34f3b9267f3ead47980f4
drbrx <80510jb@gmail.com>
2022-03-20 20:17:30 +0100

    fixed getPlayerNetAtRound() | v1.1 | @drbrx

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/data/Database.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/data/Database.java
@@ -456,7 +456,7 @@ class Profile {
             return 0;
         if(winList.get(round) < Settings.getInstance().getExtractionsPerRound())
             return getNetAtRound(round -1);
-        return Settings.getInstance().getMoneyPerWin() + getNetAtRound(round -1);
+        return Settings.getInstance().getMoneyPerWin() + getNetAtRound(round -1) -1;
 
     }
 
