b7b578a0cda91e52b414242993c2209ad54ec95e
enrico visentin <enrico04v@gmail.com>
2022-03-20 19:45:04 +0100

    fixed axis values | v1.2 | @enry04

--- Lotto/app/src/main/java/com/voltdevelopers/lotto/activities/StartGameActivity.java
+++ Lotto/app/src/main/java/com/voltdevelopers/lotto/activities/StartGameActivity.java
@@ -171,7 +171,7 @@ public class StartGameActivity extends AppCompatActivity {
         yAxis.setSpaceBottom(0);
         yAxis.setSpaceTop(0);
         yAxis.setDrawGridLines(false);
-        yAxis.setLabelCount(10, true);
+        yAxis.setLabelCount(10, false);
         yAxis.setTextColor(Color.GREEN);
         yAxis.removeAllLimitLines();
         yAxis.setAxisMaximum(100); //percentuale massima
@@ -190,7 +190,7 @@ public class StartGameActivity extends AppCompatActivity {
 
         XAxis xAxis = firstChart.getXAxis();
         xAxis.setTextColor(Color.GREEN);
-        xAxis.setLabelCount(db.getSizeSignificantPulls() / 10, true);
+        xAxis.setLabelCount(db.getSizeSignificantPulls() / 10, false);
         xAxis.removeAllLimitLines();
         xAxis.setAxisMaximum(db.getSizeSignificantPulls());
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -295,7 +295,7 @@ public class StartGameActivity extends AppCompatActivity {
         yAxis.setSpaceBottom(0);
         yAxis.setSpaceTop(0);
         yAxis.setDrawGridLines(false);
-        yAxis.setLabelCount(10, true);
+        yAxis.setLabelCount(10, false);
         yAxis.setTextColor(Color.GREEN);
         yAxis.removeAllLimitLines();
         yAxis.setAxisMaximum(1000); //soldi massimi guadagnati
@@ -314,7 +314,7 @@ public class StartGameActivity extends AppCompatActivity {
 
         XAxis xAxis = secondChart.getXAxis();
         xAxis.setTextColor(Color.GREEN);
-        xAxis.setLabelCount(db.getSizeSignificantPulls() / 10, true);
+        xAxis.setLabelCount(db.getSizeSignificantPulls() / 10, false);
         xAxis.removeAllLimitLines();
         xAxis.setAxisMaximum(db.getSizeSignificantPulls());
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
